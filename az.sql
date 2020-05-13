e433a59c7f7d1342bc9aaae4030a2c567971da9e
Dmitrii Rudik <dima.rudik@gmail.com>
2020-05-13 13:25:55 +0300

    19

--- az.sql
+++ az.sql
@@ -1,29 +1,37 @@
 -- ONLINE4 inserts: d5smkxyf2nrgc bwvvpgjnkgmxy
 col Date for a20
-col "Name of day" for a10
-col BUFFER_GETS for 99,999,999,999
-col DISK_READS for 99,999,999,999
+col "Name of day" for a11
+col "Elapsed Avg" for 99.999999
+col "BufferGets" for 99,999,999,999
+col "DiskReads" for 99,999,999,999
+col "BufferGets Avg" for 999.999
+col "DiskReads Avg" for 999.999
 var SQL_ID varchar2(256);
 exec :SQL_ID:= '&1';
 select 
+        st.plan_hash_value,
 	trunc(sn.begin_interval_time) "Date", 
 	to_char(sn.begin_interval_time,'DAY') "Name of day", 
-	round(SUM(st.elapsed_time_delta/1000000) / SUM(st.executions_delta),4) elapsed_avg, 
-	SUM(st.executions_delta) execs, 
-	SUM(st.disk_reads_delta) disk_reads, 
-	SUM(st.buffer_gets_delta) buffer_gets, 
-	count(1) snapshots_cnt 
+        SUM(st.executions_delta) "Executions",
+        round(SUM(st.elapsed_time_delta/1000000) / SUM(st.executions_delta),6) "Elapsed Avg",
+        round(SUM(st.buffer_gets_delta) / SUM(st.executions_delta),3) "BufferGets Avg",
+        round(SUM(st.disk_reads_delta) / SUM(st.executions_delta),3) "DiskReads Avg",
+	SUM(st.disk_reads_delta) "DiskReads", 
+	SUM(st.buffer_gets_delta) "BufferGets", 
+	count(1) "Snapshots"
 from 
 	dba_hist_snapshot sn, 
 	dba_hist_sqlstat st 
 where 
 	st.sql_id = :SQL_ID and 
 	sn.snap_id = st.snap_id and 
+        st.executions_delta <> 0 and
 	trunc(sn.begin_interval_time,'dd') > sysdate - 30 
 group by 
 	trunc(sn.begin_interval_time), 
+        st.plan_hash_value,
 	to_char(sn.begin_interval_time,'DAY') 
-order by 1 desc;
+order by trunc(sn.begin_interval_time) desc;
 /*
 select
         trunc(sn.begin_interval_time) "Date",
--- p1.sql
+++ p1.sql
@@ -2,7 +2,7 @@
 set numwidth 16
 --col fblker for a8
 col event for a42
-col machine for a40
+col machine for a24
 col program for a40
 col osuser for a12
 col username for a10
@@ -10,9 +10,14 @@ col sid for 999999
 col serial# for 999999
 col FBLKER for 999999
 col status for a12
+col sql_id for a13
+col sql_exec_start for a20
+col LOGON_TIME for a20
+col blking_sid for 9999999999
+col fnl_blking_sid for 9999999999
 var s number;
 exec :s := &1;
-select v.sid, v.serial#, v.event, v.p1raw, v.p1, v.p2, v.final_blocking_session fblker, v.username, v.osuser, v.status, v.machine, v.program, v.sql_id, v.sql_exec_start, v.logon_time, v.last_call_et
+select v.sid, v.serial#, v.blocking_session blking_sid, v.final_blocking_session fnl_blking_sid, v.event, v.p1raw, v.p1, v.p2, v.final_blocking_session fblker, v.username, v.osuser, v.status, v.machine, v.program, v.sql_id, v.sql_exec_start, v.logon_time, v.last_call_et
 from 
 	v$session v 
 where 
